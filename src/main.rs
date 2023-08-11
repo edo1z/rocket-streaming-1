4ce60b1e332ab90cb9eddba246e8950a1851ba69
net3i <web3ten0@gmail.com>
2023-08-11 15:31:54 +0900

    multipartデータ取得・ファイルタイプチェック

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,50 @@
 #[macro_use]
 extern crate rocket;
 
-use crate::rocket::futures::StreamExt;
+use multer::{parse_boundary, Multipart};
 use rocket::data::{Data, ToByteUnit};
+use rocket::http::ContentType;
 use tokio_util::io::ReaderStream;
 
 #[post("/", data = "<data>")]
-async fn index(data: Data<'_>) -> std::io::Result<()> {
-    let mut stream = ReaderStream::new(data.open(20.mebibytes()));
-    let mut stream_contents: Vec<u8> = Vec::new();
-    while let Some(chunk) = stream.next().await {
-        stream_contents.extend_from_slice(&chunk?);
-        println!("Received {} bytes", stream_contents.len());
+async fn index(content_type: &ContentType, data: Data<'_>) -> String {
+    let boundary = parse_boundary(content_type.to_string()).ok();
+    if boundary.is_none() {
+        println!("No boundary found");
+        return "No boundary found".to_string();
     }
-    println!("{:?}", stream_contents);
-    Ok(())
+
+    let stream = ReaderStream::new(data.open(20.mebibytes()));
+    let mut multipart = Multipart::new(stream, boundary.unwrap());
+    while let Some(mut field) = multipart.next_field().await.ok().flatten() {
+        let name = field.name();
+        let file_name = field.file_name();
+        if file_name.is_none() {
+            println!("Name: {:?}", name);
+            let value = field.text().await.ok();
+            println!("value: {:?}", value);
+        } else {
+            let content_type = field.content_type().unwrap().to_string();
+            println!("Name: {:?}, File Name: {:?}, Content-Type: {:?}", name, file_name, content_type);
+            let mut first_chunk = true;
+            while let Some(chunk) = field.chunk().await.ok().flatten() {
+                if first_chunk {
+                    first_chunk = false;
+                    let kind = infer::get(&chunk);
+                    if kind.is_none() {
+                        println!("no kind");
+                    }
+                    let mime_type = kind.unwrap().mime_type();
+                    println!("mime_type: {:?}", mime_type);
+                    if content_type != mime_type {
+                        println!("invalid file type");
+                        return "invalid file type".to_string();
+                    }
+                }
+            }
+        }
+    }
+    "Ok".to_string()
 }
 
 #[launch]
