4cebd3d04113ab3070c8cc56d8424c1c6a0dcdd9
net3i <web3ten0@gmail.com>
2023-08-11 10:23:20 +0900

    順番に取り出す

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::data::{Data, DataStream, ToByteUnit};
+use crate::rocket::futures::StreamExt;
+use rocket::data::{Data, ToByteUnit};
+use tokio_util::io::ReaderStream;
 
 #[post("/", data = "<data>")]
 async fn index(data: Data<'_>) -> std::io::Result<()> {
-    let stream: DataStream = data.open(20.mebibytes());
-    let hoge = stream.into_bytes().await?;
-    println!("bytes => {:?}", hoge.into_inner());
+    let mut stream = ReaderStream::new(data.open(20.mebibytes()));
+    let mut stream_contents: Vec<u8> = Vec::new();
+    while let Some(chunk) = stream.next().await {
+        stream_contents.extend_from_slice(&chunk?);
+        println!("Received {} bytes", stream_contents.len());
+    }
+    println!("{:?}", stream_contents);
     Ok(())
 }
 
