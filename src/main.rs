1fb889a67b77d1a8572aa5aa8efb6ae946eebb74
net3i <web3ten0@gmail.com>
2023-08-11 10:07:55 +0900

    into_bytes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket::tokio;
-use rocket::data::{Data, ToByteUnit};
+use rocket::data::{Data, DataStream, ToByteUnit};
 
 #[post("/", data = "<data>")]
 async fn index(data: Data<'_>) -> std::io::Result<()> {
-    // 最大20MBまで受け取るストリームを作成し、受け取ったデータを順次標準出力に書き出す
-    data.open(20.mebibytes()).stream_to(tokio::io::stdout()).await?;
+    let stream: DataStream = data.open(20.mebibytes());
+    let hoge = stream.into_bytes().await?;
+    println!("bytes => {:?}", hoge.into_inner());
     Ok(())
 }
 
