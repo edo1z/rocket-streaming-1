aa7c91a25d1dd81d20996ac494d954ac6b89b8a4
net3i <web3ten0@gmail.com>
2023-08-11 09:35:26 +0900

    data.open

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use rocket::data::{Data, ToByteUnit};
 
 #[post("/", data = "<data>")]
 async fn index(data: Data<'_>) -> std::io::Result<()> {
-    data.open(10.bytes()).stream_to(tokio::io::stdout()).await?;
+    // 最大20MBまで受け取るストリームを作成し、受け取ったデータを順次標準出力に書き出す
+    data.open(20.mebibytes()).stream_to(tokio::io::stdout()).await?;
     Ok(())
 }
 
